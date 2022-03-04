f7521d397efb23ca7fcfff8db7a490c981ef6ad2
Shooterah <iflox974@gmail.com>
2022-03-04 21:12:02 +0100

    TP3 DevWeb Fini

--- DevWeb/TP3/Connect.java
+++ DevWeb/TP3/Connect.java
@@ -1,8 +1,14 @@
-package devwebbdd;
+package tp3;
 import java.sql.*;
-import com.mysql.*;
+import java.time.LocalDate;
+import java.time.Period;
 
 public class Connect {
+	
+	public Connection c;
+	public Statement st;
+	public ResultSet reponse;
+	public int valideManipulation;
 	
 	public Connect() {
 		try {
@@ -13,13 +19,153 @@ public class Connect {
 		}
 		String url = "jdbc:mysql://localhost:3306/tp3bdd";
 		try {
-			Connection c = DriverManager.getConnection(url, "root", "");
+			c = DriverManager.getConnection(url, "root", "");
 			System.out.println("Connection Réussi !");
+			System.out.println("On créer le statement...");
+			st = c.createStatement();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			System.out.println("Connection Échoué !");
 		}
 	}
+	
+	public void close() throws SQLException {
+		System.out.println("On ferme la connection et le statement...");
+		st.close();
+		c.close();
+	}
+	
+	public void reqSQL(String query, char type) throws SQLException{
+		if(type != 's' && type != 'm'){
+			System.out.println("Erreur dans le type, attendu : [s ou m] et vous avez rentrez :[" + type + "]");
+		}
+		else if(type == 's'){
+			reponse = st.executeQuery(query);
+			afficheReqIntero(query);
+		}
+		else{
+			valideManipulation = st.executeUpdate(query);
+		}
+	}
+	
+	public void afficheReqIntero(String maRequete) throws SQLException {
+		ResultSetMetaData metadata = reponse.getMetaData();
+		String nomColonne = "| ";
+		for (int i = 0; i < metadata.getColumnCount(); i++) {
+			nomColonne += metadata.getColumnName(i+1) + " | ";
+		}
+		System.out.println();
+		System.out.println("Requete : [" + maRequete +"]");
+		System.out.println();
+		System.out.println(nomColonne);
+		System.out.println();
+		String affiche = "| ";
+		while(reponse.next()) {
+			for (int i = 0; i < metadata.getColumnCount(); i++) {
+				affiche += reponse.getString(i+1) + " | ";
+			}
+			System.out.println(affiche);
+			affiche = "| ";
+		}
+		System.out.println();
+	}
+	
+	public void execReqManip(int choixReq) throws SQLException {
+		String maRequete;
+		switch(choixReq){
+			case 1:
+				maRequete = "INSERT INTO utilisateur (Nom, Prenom, DateNaissance, Login, Mdp) VALUES ('Pereira', 'Jeoffrey', '1999-07-08', 'Godefroy', 'blabla')";
+				this.reqSQL(maRequete, 'm');
+				break;
+			case 2:
+				maRequete = "INSERT INTO utilisateur (Nom, Prenom, DateNaissance, Login, Mdp) VALUES ('Hauf', 'Jocelin', '1999-08-08', 'Jojo', 'boubou')";
+				this.reqSQL(maRequete, 'm');
+				break;
+			case 3:
+				maRequete = "INSERT INTO utilisateur (Nom, Prenom, DateNaissance, Login, Mdp) VALUES ('Rome', 'Sylvain', '2000-01-08', 'Xeos', 'foufou')";
+				this.reqSQL(maRequete, 'm');
+				break;
+			case 4:
+				maRequete = "DELETE FROM utilisateur WHERE Nom LIKE 'Gonzalez'";
+				this.reqSQL(maRequete, 'm');
+				break;
+			case 5:
+				maRequete = "DELETE FROM utilisateur WHERE Prenom LIKE 'Sylvain'";
+				this.reqSQL(maRequete, 'm');
+				break;
+		}
+	}
+	
+	public void execReqIntero(int choixReq) throws SQLException {
+		String maRequete;
+		switch(choixReq){
+			case 1:
+				maRequete = "SELECT * FROM utilisateur";
+				this.reqSQL(maRequete, 's');
+				break;
+			case 2:
+				maRequete = "SELECT * FROM utilisateur WHERE Nom LIKE 'Doffemont'";
+				this.reqSQL(maRequete, 's');
+				break;
+			case 3:
+				maRequete = "SELECT * FROM communaute";
+				this.reqSQL(maRequete, 's');
+				break;
+			case 4:
+				maRequete = "SELECT * FROM utilisateur WHERE Prenom LIKE 'Mathieu'";
+				this.reqSQL(maRequete, 's');
+				break;
+			case 5:
+				maRequete = "SELECT * FROM utilisateur WHERE Login LIKE 'Shooterah'";
+				this.reqSQL(maRequete, 's');
+				break;
+		}
+	}
+	
+	public void inscriptionRandom() throws SQLException{
+		
+		int nbLettreP;
+		int nbLettreN;
+		String Nom = "";
+		String Prenom = "";
+		String maRequete = "";
+		for(int i = 0; i < 10000; i++) {
+			nbLettreP = (int)(3 + (Math.random() * 10)); /* Prénom Entre 3 et 10 Lettre */
+			nbLettreN = (int)(3 + (Math.random() * 10)); /* Nom Entre 3 et 10 Lettre */
+			for(int y = 0; y < nbLettreP; y++) {
+				Prenom += (char)(65 + (int)(Math.random() * (91 - 65))); /* Lettre en a et z */
+			}
+			for(int y = 0; y < nbLettreN; y++) {
+				Nom += (char)(65 + (int)(Math.random() * (91 - 65))); /* Lettre en a et z */
+			}
+			maRequete = "INSERT INTO utilisateur (Nom, Prenom) VALUES ('" + Nom + "', '" + Prenom + "')";
+			this.reqSQL(maRequete, 'm');
+			Nom = "";
+			Prenom = "";
+			maRequete = "";
+		}
+	}
+	
+	public int getAge(int id_personne) throws SQLException{
+		
+		LocalDate today = LocalDate.now();
+		
+		reponse = st.executeQuery("SELECT DateNaissance FROM utilisateur WHERE Id = " + id_personne + "");
+		
+		while(reponse.next()){
+			if(reponse.getDate(1) != null) {
+				LocalDate anniv = Date.valueOf(reponse.getDate(1).toString()).toLocalDate();
+				return  Period.between(anniv, today).getYears();
+			}
+			else {
+				return 0;
+			}
+		}
+		
+		return 0;
+	}
+	
+	
 
 }
--- DevWeb/TP3/Test.java
+++ DevWeb/TP3/Test.java
@@ -1,13 +1,102 @@
-package devwebbdd;
-import java.sql.*;
-import com.mysql.*;
+package tp3;
+
+import java.sql.SQLException;
 
 public class Test {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws SQLException {
 		
-		Connect maConnexion = new Connect();
+		int choixTypeReq = -1;
+		int choixReq = -1;
+		String requete = "";
+		
+		/* Test nb arguments */
+		if(args.length < 1) {
+			System.out.println("Usage : java -jar Test [reqInterr OU reqManip OU reqLibre OU reqRandom OU reqAge] [1-5 OU si reqLibre votre requete]");
+			System.exit(0);
+		}
+		
+		/* Test arguments type de requete */
+		if(!(args[0].equals("reqInterr")) && !(args[0].equals("reqManip")) && !(args[0].equals("reqLibre")) && !(args[0].equals("reqRandom")) && !(args[0].equals("reqAge"))){
+			System.out.println(args[0]);
+			System.out.println("Usage : java -jar Test [reqInterr OU reqManip OU reqLibre OU reqRandom OU reqAge] [1-5 OU si reqLibre votre requete]");
+			System.exit(0);
+		}
+		
+		/* reqInterr */
+		if(args[0].equals("reqInterr")) { 
+			if(Integer.parseInt(args[1]) < 0 || Integer.parseInt(args[1]) > 5) {
+				System.out.println("Usage : java -jar Test [reqInterr OU reqManip OU reqLibre OU reqRandom OU reqAge] [1-5 OU si reqLibre votre requete]");
+				System.exit(0);
+			}
+			else {
+				choixReq = Integer.parseInt(args[1]);
+				Connect maConnexion = new Connect();
+				maConnexion.execReqIntero(choixReq);
+			}		
+		}
+		else if(args[0].equals("reqManip")) { /* reqManip */
+			if(Integer.parseInt(args[1]) < 0 || Integer.parseInt(args[1]) > 5) {
+				System.out.println("Usage : java -jar Test [reqInterr OU reqManip OU reqLibre OU reqRandom OU reqAge] [1-5 OU si reqLibre votre requete]");
+				System.exit(0);
+			}
+			else {
+				choixReq = Integer.parseInt(args[1]);
+				Connect maConnexion = new Connect();
+				maConnexion.execReqManip(choixReq);
+			}		
+		}
+		else if(args[0].equals("reqManip")){ /* reqLibre */
+			if(!(args[1].equals("SELECT")) && !(args[1].equals("UPDATE")) && !(args[1].equals("INSERT")) && !(args[1].equals("DELETE"))) {
+				System.out.println("Erreur : Vous ne pouvez utiliser que INSERT / UPDATE / DELETE et SELECT");
+				System.exit(0);
+			}
+			else if(args[1].equals("SELECT")){
+				choixTypeReq = 1;
+			}
+			else {
+				choixTypeReq = 2;
+			}
+			
+			for(int i = 1; i < args.length-1; i++) {
+				requete += args[i] + " ";
+			}
+			requete += args[args.length-1];
+			
+			Connect maConnexion = new Connect();
+			
+			System.out.println("On éxécute la requête : [" + requete + "]");
+			
+			switch(choixTypeReq){
+				case 1: /* SELECT */
+					maConnexion.reqSQL(requete, 's');
+					break;
+				case 2: /* INSERT / UPDATE / DELETE */
+					maConnexion.reqSQL(requete, 'm');
+				break;
+			}
+			maConnexion.close();
+		}
+		else if(args[0].equals("reqRandom")){ /* reqRandom */
+			Connect maConnexion = new Connect();
+			System.out.println("On créer 10 000 utilisateurs de façon random, ça prend quelques secondes...");
+			maConnexion.inscriptionRandom();
+			maConnexion.close();
+		}
+		else { /* reqAge */
+			int idUtil = 0;
+			if(args.length != 2) {
+				System.out.println("Usage : java -jar Test [reqInterr OU reqManip OU reqLibre OU reqRandom OU reqAge] [1-5 OU si reqLibre votre requete]");
+				System.exit(0);
+			}
+			else {
+				idUtil = Integer.parseInt(args[1]);
+			}
+			Connect maConnexion = new Connect();
+			System.out.println("Calcule de l'age de l'utilisateur n°" + idUtil + "");
+			System.out.println("il a [" + maConnexion.getAge(idUtil) + "] ans");
+			maConnexion.close();
+		}
 	}
 
 }
