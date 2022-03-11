ff7b3f0ab484797b064791043101fb4f78deab04
Shooterah <iflox974@gmail.com>
2022-03-11 19:01:44 +0100

    Avancé TP4 DevWeb

--- DevWeb/TP4/Tchatche.java
+++ DevWeb/TP4/Tchatche.java
@@ -3,6 +3,8 @@ package tchatche;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Hashtable;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,7 +18,6 @@ import javax.servlet.http.HttpSession;
 @WebServlet("/Tchatche")
 public class Tchatche extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	ArrayList<Message> listeMessage = new ArrayList<Message>();
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -29,25 +30,30 @@ public class Tchatche extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+	@SuppressWarnings("unchecked")
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		HttpSession session = request.getSession(true); /* On récup la session */
 		String Pseudo = (String)session.getAttribute("Pseudo");
 	    /* On affiche le Pseudo */
 		response.getWriter().println("<h1>Bonjour " + Pseudo + "</h1><br><br>");
+		/* On récup la liste des message dans le context */
+		ArrayList<Message> tmpListeMessage = (ArrayList<Message>)getServletContext().getAttribute("listeMessage");
 		/* On affiche la liste des messages */
-		for(int i = 0; i < listeMessage.size(); i++) {
+		for(int i = 0; i < tmpListeMessage.size(); i++) {
 			String ligne = "";
-			ligne += listeMessage.get(i).dateMessage;
-			ligne += " " + listeMessage.get(i).Pseudo;
-			ligne += " : " + listeMessage.get(i).Texte;
+			ligne += tmpListeMessage.get(i).dateMessage;
+			ligne += " " + tmpListeMessage.get(i).Pseudo;
+			ligne += " : " + tmpListeMessage.get(i).Texte;
 			response.getWriter().println(ligne + "<br>");
 		}
+		response.setIntHeader("Refresh", 1);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
+	@SuppressWarnings("unchecked")
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession(true); /* On récup la session */
 		/* On setup le message */
@@ -55,7 +61,17 @@ public class Tchatche extends HttpServlet {
 		String Pseudo = (String)session.getAttribute("Pseudo");
 		LocalDate maintenant = LocalDate.now();
 		Message mess = new Message(Pseudo, textMessage, maintenant);
-		listeMessage.add(mess);
+		/* On récup la liste des message et la HashTable des Utilisateurs dans le context */
+		ArrayList<Message> tmpListeMessage = (ArrayList<Message>)getServletContext().getAttribute("listeMessage");
+		Hashtable<String, Integer> tmpListeUtil = (Hashtable<String, Integer>)getServletContext().getAttribute("listeUtil");
+		/* On ajoute le Message */
+		tmpListeMessage.add(mess);
+		/* On met a jour la HashTable Utilisateur (+1 au nb de message posté) */
+		tmpListeUtil.put(Pseudo, tmpListeUtil.get(Pseudo) + 1);
+		/* On remet tout dans le context */
+		getServletContext().setAttribute("listeUtil", tmpListeUtil);
+		getServletContext().setAttribute("listeMessage", tmpListeMessage);
+		/* On redirige */
 		response.sendRedirect("tchatche.html");
 		doGet(request, response);
 	}
--- DevWeb/TP4/init.java
+++ DevWeb/TP4/init.java
@@ -1,6 +1,12 @@
 package tchatche;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Map;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -16,21 +22,43 @@ public class init extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
+     * @throws ServletException 
      * @see HttpServlet#HttpServlet()
      */
-    public init() {
-        super();
-        // TODO Auto-generated constructor stub
+	
+	public init() throws ServletException {
+		super();
+	}
+	
+	
+	public init(ServletConfig config) throws ServletException {
+        super.init(config);
+        ServletContext servletContext = this.getServletContext();
+        /* On déclare la liste des Utilisateur et des Messages et on les met dans le context */
+        ArrayList<Message> listeMessage = new ArrayList<Message>();
+        Hashtable<String, Integer> listeUtil = new Hashtable<String, Integer>();
+        /* On enregistre les 2 listes dans le context */
+        servletContext.setAttribute("listeMessage", listeMessage);
+        servletContext.setAttribute("listeUtil", listeUtil);
+        
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+	@SuppressWarnings("unchecked")
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		ServletContext servletContext = request.getServletContext();
 		String Pseudo = request.getParameter("Pseudo");
 		HttpSession session = request.getSession(true);
 		session.setAttribute("Pseudo", Pseudo);
+		/* On récupère la HashTable des Utilisateur et */ /* On rajoute l'utilisateur */
+		Hashtable<String, Integer> tmpListeUtil = (Hashtable<String, Integer>) servletContext.getAttribute("listeUtil");
+		/* On rajoute l'utilisateur */
+		tmpListeUtil.put(Pseudo, 0);
+		/* On met a jour le context */
+		servletContext.setAttribute("listeUtil", tmpListeUtil);
+		 /* On redirige */
 		response.sendRedirect("tchatche.html");
 	}
 
